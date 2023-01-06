6652fc2055669859f8f559083ec0deb39699fbbd
dylan <64336537+dylann123@users.noreply.github.com>
2023-01-06 10:37:24 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,18 +1,14 @@
 const express = require("express")
 const app = express()
 app.use(express.json())
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Headers", "*");
-  next();
-});
-
 let phrase = "none"
 
 app.get("/", (req, res) => {
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
     res.send(phrase)
 })
 app.post("/", (req, res) => {
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
     phrase = req.body.xpath
     console.log(req.body.xpath);
     res.send("success")
