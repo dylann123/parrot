1204d7db1c102c2fe186b7e5edab218eed72c0d6
dylan <64336537+dylann123@users.noreply.github.com>
2023-01-06 10:12:06 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require("express")
-const cors = require("cors")
 const app = express()
 app.use(express.json())
-app.use(cors({
-    methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH'],
-    origin: "*"
-}));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 let phrase = "none"
 
